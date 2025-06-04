45632aca9e7fca8b00aafdbeb53a8738f711ede6
ChuanHoang2021 <chuanhoang0969151953@gmail.com>
2025-06-04 16:32:34 +0700

    ma nguon dau tien

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ChuanHoangUtilLive
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            byte i = 0;
+            do
+            {
+                Console.Write($"\rLoading...{i}%...in other thread!");
+                await Task.Delay(100);
+                i++;
+            }
+            while (i < 100);
+            Console.WriteLine();
+            Console.WriteLine("End.");
+        }
+    }
+}
