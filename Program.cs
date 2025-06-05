ff5f0a77e5f1e2e6b8f33e6dbbeb91aebd59267a
ChuanHoang2021 <chuanhoang0969151953@gmail.com>
2025-06-05 13:42:06 +0700

    Thay doi ma nguon Program.cs

--- Program.cs
+++ Program.cs
@@ -2,31 +2,20 @@
 
 namespace ChuanHoangUtilLive
 {
+    using TestNameSpace;
     class Program
     {
         static void Main()
         {
-            Person p1;
-            p1.Age = 18;
-            p1.Name = "Chuan";
-            Person p2 = p1;
-            p2.Age = 20;
-            p2.Name = "Chuan Hoang";
-            var r1 = p1.ShowInfo();
-            var r2 = p2.ShowInfo();
-            Console.WriteLine(r1);
-            Console.WriteLine(r2);
+            TestClass.Hello();
         }
     }
-    struct Person
+}
+
+namespace TestNameSpace
+{
+    class TestClass
     {
-        public byte Age;
-        public string Name;
-        public Person(byte age, string name)
-        {
-            Age = age;
-            Name = name;
-        }
-        public string ShowInfo() => $"{Name} | {Age}";
+        public static void Hello() => Console.WriteLine("Hello from Test!");
     }
 }
