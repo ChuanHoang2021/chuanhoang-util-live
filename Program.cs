9468a7e3f0375951462f8f6dc397c70d836dd83b
ChuanHoang2021 <chuanhoang0969151953@gmail.com>
2025-06-05 13:19:54 +0700

    thay doi toan bo ma nguon su

--- Program.cs
+++ Program.cs
@@ -1,22 +1,32 @@
 ﻿using System;
-using System.Threading.Tasks;
 
 namespace ChuanHoangUtilLive
 {
     class Program
     {
-        static async Task Main()
+        static void Main()
         {
-            byte i = 0;
-            do
-            {
-                Console.Write($"\rLoading something...{i}%...in other thread!");
-                await Task.Delay(100);
-                i++;
-            }
-            while (i < 100);
-            Console.WriteLine();
-            Console.WriteLine("End of main thread.");
+            Person p1;
+            p1.Age = 18;
+            p1.Name = "Chuan";
+            Person p2 = p1;
+            p2.Age = 20;
+            p2.Name = "Chuan Hoang";
+            var r1 = p1.ShowInfo();
+            var r2 = p2.ShowInfo();
+            Console.WriteLine(r1);
+            Console.WriteLine(r2);
         }
+    }
+    struct Person
+    {
+        public byte Age;
+        public string Name;
+        public Person(byte age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+        public string ShowInfo() => $"{Name} | {Age}";
     }
 }
