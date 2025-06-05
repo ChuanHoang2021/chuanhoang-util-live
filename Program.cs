ac5f2d202fcb36b81131cb044ce47f51cd6e36f1
ChuanHoang2021 <chuanhoang0969151953@gmail.com>
2025-06-05 14:22:00 +0700

    Thuc hanh kieu vo danh vaf Linq

--- Program.cs
+++ Program.cs
@@ -1,21 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChuanHoangUtilLive
 {
-    using TestNameSpace;
     class Program
     {
         static void Main()
         {
-            TestClass.Hello();
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie {Name = "Tien nghich", Category = "Donghua", Imdb = 8, Studio = "ChinaStudio", Year = 2023},
+                new Movie {Name = "Endgame", Category = "Superhero", Imdb = 9, Studio = "Marvel", Year  = 2018},
+                new Movie {Name = "Game of Throne", Category = "Fantasy", Imdb = 7, Studio = "Unknown", Year = 2020},
+                new Movie {Name = "The Lord of the rings", Category = "Fantasy", Imdb = 9, Studio = "Hollywood", Year = 2001},
+                new Movie {Name = "Vo lam truyen ky", Category = "Kiem hiep", Imdb = 7, Studio = "ChinaStudio", Year = 2005},
+                new Movie {Name = "Cuu am chan kinh", Category = "Game", Imdb = 8, Studio = "SnailGame", Year = 2013},
+            };
+            var fillterMovies = movies.Where(m => m.Category == "Fantasy");
+            foreach (var m in fillterMovies)
+                Console.WriteLine($"Name = {m.Name}");
         }
     }
-}
-
-namespace TestNameSpace
-{
-    class TestClass
+    class Movie
     {
-        public static void Hello() => Console.WriteLine("Hello from Test!");
+        public string Name { set; get; }
+        public string Category { set; get; }
+        public byte Imdb { set; get; }
+        public string Studio { set; get; }
+        public int Year { set; get; }
     }
 }
